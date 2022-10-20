d68b4c5bf826e0fb35cf2ade6305fb91e4b31230
rick0x00 <rick.0x00@gmail.com>
2022-10-19 23:17:35 -0300

    Add Password Variables

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PassRootDB="zabbix"
+PassZABBIXDB="zabbix"
+
 wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4%2Bdebian11_all.deb
 dpkg -i zabbix-release_6.0-4+debian11_all.deb
 apt update
@@ -11,14 +14,14 @@ apt install mariadb-client mariadb-server
 #mysql -uroot -p
 mysql -e "SET PASSWORD FOR 'root'@localhost = PASSWORD("$PassRootDB");"
 mysql -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
-mysql -e "create user zabbix@localhost identified by 'password';"
+mysql -e "create user zabbix@localhost identified by '$PassZABBIXDB';"
 mysql -e "grant all privileges on zabbix.* to zabbix@localhost;"
 mysql -e "FLUSH PRIVILEGES;"
 
-zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p"$PassRootDB" zabbix
 
 nano /etc/zabbix/zabbix_server.conf
-echo "DBPassword=password"
+echo "DBPassword=$PassZABBIXDB"
 
 locale-gen "en_US.UTF-8"
 
